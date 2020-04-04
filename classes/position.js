9f0b293ee51ed43d96a905f3356c7f95bae3d59c
parent-tobias <parent.tobias@gmail.com>
2020-04-04 17:10:26 -0400

    Error in taxes and fees calculation, in the Position. Fixed.

--- classes/position.js
+++ classes/position.js
@@ -2,6 +2,7 @@ import Transaction from './transaction.js';
 
 // a utility function we can use to total each property as we want.
 const sumBy = prop => (total, transaction) => transaction.isSellOrder ? total + transaction[prop] : total - transaction[prop];
+const absoluteSum = prop => (total, transaction) => total + transaction[prop];
 
 export default class Position {
   constructor({
@@ -27,25 +28,25 @@ export default class Position {
     return this.state.transactions.reduce(sumBy("gross"), 0);
   }
   get brokerage(){
-    return this.state.transactions.reduce(sumBy("brokerage"), 0);
+    return this.state.transactions.reduce(absoluteSum("brokerage"), 0);
   }
   get STT(){
-    return this.state.transactions.reduce(sumBy("STT"), 0);
+    return this.state.transactions.reduce(absoluteSum("STT"), 0);
   }
   get transactionFee(){
-    return this.state.transactions.reduce(sumBy("transactionFee"), 0)
+    return this.state.transactions.reduce(absoluteSum("transactionFee"), 0)
   }
   get GST(){
-    return this.state.transactions.reduce(sumBy("GST"), 0)
+    return this.state.transactions.reduce(absoluteSum("GST"), 0)
   }
   get SEBI(){
-    return this.state.transactions.reduce(sumBy("brokerage"), 0)
+    return this.state.transactions.reduce(absoluteSum("SEBI"), 0)
   }
   get stampCharge(){
-    return this.state.transactions.reduce(sumBy("stampCharge"), 0)
+    return this.state.transactions.reduce(absoluteSum("stampCharge"), 0)
   }
   get totalFees(){
-    return this.state.transactions.reduce(sumBy("totalFees"), 0)
+    return this.state.transactions.reduce(absoluteSum("totalFees"), 0)
   }
   get net(){
     return this.state.transactions.reduce(sumBy("net"), 0)
