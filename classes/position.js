54610c64f4c650606aa83cc0c298a2b1f68a2d66
parent-tobias <parent.tobias@gmail.com>
2020-04-04 17:39:14 -0400

    updated Position calculations to show two-decimal precision.

--- classes/position.js
+++ classes/position.js
@@ -3,6 +3,8 @@ import Transaction from './transaction.js';
 // a utility function we can use to total each property as we want.
 const sumBy = prop => (total, transaction) => transaction.isSellOrder ? total + transaction[prop] : total - transaction[prop];
 const absoluteSum = prop => (total, transaction) => total + transaction[prop];
+const toPrecision = precision => num => Math.round(num*(Math.pow(10, precision))/Math.pow(10, precision));
+const currency = toPrecision(2);
 
 export default class Position {
   constructor({
@@ -25,30 +27,30 @@ export default class Position {
     return this.transactions.find(transaction => transaction.time == dateStamp);
   }
   get gross(){
-    return this.state.transactions.reduce(sumBy("gross"), 0);
+    return currency( this.state.transactions.reduce(sumBy("gross"), 0) );
   }
   get brokerage(){
-    return this.state.transactions.reduce(absoluteSum("brokerage"), 0);
+    return currency( this.state.transactions.reduce(absoluteSum("brokerage"), 0) );
   }
   get STT(){
-    return this.state.transactions.reduce(absoluteSum("STT"), 0);
+    return currency( this.state.transactions.reduce(absoluteSum("STT"), 0) );
   }
   get transactionFee(){
-    return this.state.transactions.reduce(absoluteSum("transactionFee"), 0)
+    return currency ( this.state.transactions.reduce(absoluteSum("transactionFee"), 0) );
   }
   get GST(){
-    return this.state.transactions.reduce(absoluteSum("GST"), 0)
+    return currency( this.state.transactions.reduce(absoluteSum("GST"), 0) )
   }
   get SEBI(){
-    return this.state.transactions.reduce(absoluteSum("SEBI"), 0)
+    return currency( this.state.transactions.reduce(absoluteSum("SEBI"), 0) )
   }
   get stampCharge(){
-    return this.state.transactions.reduce(absoluteSum("stampCharge"), 0)
+    return currency( this.state.transactions.reduce(absoluteSum("stampCharge"), 0) )
   }
   get totalFees(){
-    return this.state.transactions.reduce(absoluteSum("totalFees"), 0)
+    return currency( this.state.transactions.reduce(absoluteSum("totalFees"), 0) )
   }
   get net(){
-    return this.state.transactions.reduce(sumBy("net"), 0)
+    return currency( this.state.transactions.reduce(sumBy("net"), 0) );
   }  
 }
